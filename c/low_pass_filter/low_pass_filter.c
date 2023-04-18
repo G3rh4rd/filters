62f3d4d2eac7dd7f0323dc253dd6a5d53e384afd
GeDi <einkauf@heero-sports.de>
2023-04-18 14:18:05 +0200

    added low pass filter implementation in c

---
+++ c/low_pass_filter/low_pass_filter.c
@@ -0,0 +1,16 @@
+#include "low_pass_filter.h"
+
+void init_low_pass_filter(low_pass_filter_t* low_pass_filter, float alpha, float initial_val) {
+    // clamp alpha to make sure it's between 0 and 1
+    if(alpha > 1.f) alpha = 1.f;
+    else if(alpha < 0.f) alpha = 0.f;
+
+    // initialize object with it's initial values
+    low_pass_filter->alpha = alpha;
+    low_pass_filter->value = initial_val;
+}
+
+void process_low_pass_filter(low_pass_filter_t* low_pass_filter, float val) {
+    float filtered_val = (low_pass_filter->alpha * low_pass_filter->value) + ((1.f - low_pass_filter->alpha) * val);
+    low_pass_filter->value = filtered_val;
+}
---
+++ c/low_pass_filter/low_pass_filter.h
@@ -0,0 +1,13 @@
+#ifndef LOW_PASS_FILTER_H
+#define LOW_PASS_FILTER_H
+
+typedef struct {
+    float alpha;
+    float value;
+} low_pass_filter_t;
+
+void init_low_pass_filter(low_pass_filter_t* low_pass_filter, float alpha, float initial_val);
+
+void process_low_pass_filter(low_pass_filter_t* low_pass_filter, float val);
+
+#endif /* LOW_PASS_FILTER_H */
